1cb679cefcc60762fae48f8254bcaa745934a9a2
Sonia Sabbahi <soniadevpro@gmail.com>
2025-02-17 16:10:41 +0100

    init projet php

---
+++ index.php
@@ -0,0 +1,98 @@
+<?php
+    # Inclusion du header
+    require_once 'partials/header.php';
+?>
+
+<!-- Contenu -->
+<main>
+    <!-- .p-3.mx-auto.text-center>h1.display-4{Actu360} -->
+    <div class="p-5 mx-auto text-center">
+        <h1 class="display-4">Actu360&deg;</h1>
+    </div>
+
+    <!-- .py-5.bg-light>.container>.row>.mb-4.col-md-4*6>.card.shadow-sm>img.img-fluid+.card-body>h5.card-title{Titre $}+small.text-muted{Auteur $}+p.card-text{Contenu $}+a.btn.btn-primary{Lire la suite} -->
+    <div class="py-5 bg-light">
+        <div class="container">
+            <div class="row">
+
+                <div class="mb-4 col-md-4 col-sm-6">
+                    <div class="card shadow-sm">
+                        <img src="https://fakeimg.pl/600x350/?text=Actu360" alt="" class="img-fluid">
+                        <div class="card-body">
+                            <h5 class="card-title">Titre 1</h5>
+                            <small class="text-muted">Auteur 1</small>
+                            <p class="card-text">Contenu 1</p>
+                            <a href="" class="btn btn-primary">Lire la suite</a>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="mb-4 col-md-4 col-sm-6">
+                    <div class="card shadow-sm">
+                        <img src="https://fakeimg.pl/600x350/?text=Actu360" alt="" class="img-fluid">
+                        <div class="card-body">
+                            <h5 class="card-title">Titre 2</h5>
+                            <small class="text-muted">Auteur 2</small>
+                            <p class="card-text">Contenu 2</p>
+                            <a href="" class="btn btn-primary">Lire la suite</a>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="mb-4 col-md-4 col-sm-6">
+                    <div class="card shadow-sm">
+                        <img src="https://fakeimg.pl/600x350/?text=Actu360" alt="" class="img-fluid">
+                        <div class="card-body">
+                            <h5 class="card-title">Titre 3</h5>
+                            <small class="text-muted">Auteur 3</small>
+                            <p class="card-text">Contenu 3</p>
+                            <a href="" class="btn btn-primary">Lire la suite</a>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="mb-4 col-md-4 col-sm-6">
+                    <div class="card shadow-sm"><img src="https://fakeimg.pl/600x350/?text=Actu360" alt="" class="img-fluid">
+                        <div class="card-body">
+                            <h5 class="card-title">Titre 4</h5>
+                            <small class="text-muted">Auteur 4</small>
+                            <p class="card-text">Contenu 4</p>
+                            <a href="" class="btn btn-primary">Lire la suite</a>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="mb-4 col-md-4 col-sm-6">
+                    <div class="card shadow-sm">
+                        <img src="https://fakeimg.pl/600x350/?text=Actu360" alt="" class="img-fluid">
+                        <div class="card-body">
+                            <h5 class="card-title">Titre 5</h5>
+                            <small class="text-muted">Auteur 5</small>
+                            <p class="card-text">Contenu 5</p>
+                            <a href="" class="btn btn-primary">Lire la suite</a>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="mb-4 col-md-4 col-sm-6">
+                    <div class="card shadow-sm">
+                        <img src="https://fakeimg.pl/600x350/?text=Actu360" alt="" class="img-fluid">
+                        <div class="card-body">
+                            <h5 class="card-title">Titre 6</h5>
+                            <small class="text-muted">Auteur 6</small>
+                            <p class="card-text">Contenu 6</p>
+                            <a href="" class="btn btn-primary">Lire la suite</a>
+                        </div>
+                    </div>
+                </div>
+
+            </div> <!-- /.row -->
+        </div> <!-- /.container -->
+    </div> <!-- /.bg-light -->
+
+</main>
+
+<?php
+# Inclusion du footer
+require_once 'partials/footer.php';
+?>
---
+++ partials/header.php
@@ -0,0 +1,52 @@
+<!doctype html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Actu360&deg;</title>
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
+          integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+</head>
+<body>
+
+<!-- En-tête -->
+<header>
+    <nav class="navbar navbar-expand-lg bg-body-tertiary">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="#">Actu360&deg;</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText"
+                    aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarText">
+                <ul class="navbar-nav text-center mx-auto mb-2 mb-lg-0">
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="#">Accueil</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Politique</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Economie</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Culture</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Sports</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Loisirs</a>
+                    </li>
+                </ul>
+                <div class="text-right">
+                    <a class="nav-item btn btn-outline-info" href="#">Connexion</a>
+                    <a class="nav-item btn btn-outline-warning" href="#">Inscription</a>
+                </div>
+            </div>
+        </div>
+    </nav>
+</header>
